094ca70460901970518902451e05bbfed8368f84
Chris Mendez <mail@chrisaiv.com>
2017-02-19 12:43:48 -0800

    Adding a Dashboard and HeroDetail through a routing module

--- Basics/app-02/src/app/hero-detail/hero-detail.component.ts
+++ Basics/app-02/src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
+//We use the params observable to extract the id parameter value from the ActivatedRoute
+import { ActivatedRoute, Params }   from '@angular/router';
+//browser's history stack using the Location
+import { Location }                 from '@angular/common';
+//Switch map operator is used with route parameters
+import 'rxjs/add/operator/switchMap';
 
 import { Hero } from '../classes/hero';
+import { HeroService } from '../services/hero.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
@@ -12,7 +20,21 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  goBack(): void {
+    this.location.back();
+  }
 
-  ngOnInit() { }
+  constructor(
+	  private heroService: HeroService,
+	  private route: ActivatedRoute,
+	  private location: Location
+	) {}
+
+  ngOnInit() {
+   this.route.params
+	  //maps the id in the observable route parameters to a new Observable
+	  //If the user re-navigates to this component while a getHero request is still inflight, switchMap cancels that old request before calling HeroService.getHero again.
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
 }
--- Basics/app-02/src/app/services/hero.service.ts
+++ Basics/app-02/src/app/services/hero.service.ts
@@ -5,6 +5,11 @@ import { HEROES } from '../data/mock-heroes';
 
 @Injectable()
 export class HeroService {	
+	
+	getHero(id: number): Promise<Hero> {
+		return this.getData().then(heroes => heroes.find(hero => hero.id === id));
+	}
+	
 	getData(): Promise<Hero[]> {
 		//Use Promise to get mock data, then return an array of mock data.
 		return Promise.resolve(HEROES);

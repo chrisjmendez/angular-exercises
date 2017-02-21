ceb92fcb2ca8a1fa1d92929626ca1cbff06ffbca
Chris Mendez <mail@chrisaiv.com>
2017-02-20 21:26:10 -0800

    Removed the mock data and updated it with NodeJS api server data

--- Basics/app-02/src/app/services/hero.service.ts
+++ Basics/app-02/src/app/services/hero.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from '../classes/hero';
-import { HEROES } from '../data/mock-heroes';
 
 @Injectable()
 export class HeroService {	
+
+	private url = 'http://localhost:8080/heroes'
 	
 	getHero(id: number): Promise<Hero> {
 		return this.getData().then(heroes => heroes.find(hero => hero.id === id));
 	}
 	
+	//Old Method
+	// getData(): Promise<Hero[]> {
+	// 	//Use Promise to get mock data, then return an array of mock data.
+	// 	return Promise.resolve(HEROES);
+	// }
+	
+	//New Method
 	getData(): Promise<Hero[]> {
-		//Use Promise to get mock data, then return an array of mock data.
-		return Promise.resolve(HEROES);
+		return this.http.get(this.url)
+			.toPromise()
+			.then(response => response.json().data as Hero[])
+			.catch(this.onError);
 	}
 	
 	getDataSlowly(): Promise<Hero[]> {
@@ -20,5 +32,10 @@ export class HeroService {
 	    // Simulate server latency with 2 second delay
 	    setTimeout(() => resolve(this.getData()), 500);
 	  });
+	}
+	
+	private onError(error:any): Promise<any> {
+		console.error("An error occured", error);
+		return Promise.reject(error.message || error);
 	}
 }

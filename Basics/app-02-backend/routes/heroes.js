dd5bc5223a8dc3aefdb2a82a707e6eee36f0ea00
Chris Mendez <mail@chrisaiv.com>
2017-03-02 21:39:34 -0800

    Refactored the model

--- Basics/app-02-backend/routes/heroes.js
+++ Basics/app-02-backend/routes/heroes.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var _ = require('lodash'); 
+var _ = require('lodash');
 
-var data = [
-	{ id: 11, name: 'Mr. Nice' },
-	{ id: 12, name: 'Narco' },
-	{ id: 13, name: 'Bombasto' },
-	{ id: 14, name: 'Celeritas' },
-	{ id: 15, name: 'Magneta' },
-	{ id: 16, name: 'RubberMan' },
-	{ id: 17, name: 'Dynama' },
-	{ id: 18, name: 'Dr IQ' },
-	{ id: 19, name: 'Magma' },
-	{ id: 20, name: 'Tornado' }
-]
+var data = require('../public/javascripts/json/Model');
 
 /* GET home page. */
 
